120ddd71373df45dbdb137cca3f1778df88532e9
Max L <122556386+maaxli@users.noreply.github.com>
2023-12-29 21:50:34 -0500

    User route CRUD with prisma

--- README.md
+++ README.md
@@ -3,6 +3,8 @@
 Built with Express, Prisma, and SQLite
 
 ## To-dos:
-- Integrate app with MySQL or MongoDB without ORM
+- Implement authentication flow
+- Protect private routes (user and tweet)
 - Implement backend for tweet feed
 - Implement cookies (for JWT)?
+- Integrate app with MySQL or MongoDB without ORM
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,38 +1,105 @@
 const asyncHandler = require('express-async-handler');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
 
 //@desc Get all users
 //@route GET /users
 //@access public
 const getUsers = asyncHandler(async(req, res, err) => {
-    res.json({ message: "route working!" });
+    const allUsers = await prisma.user.findMany({
+        select: {
+            id: true,
+            name: true,
+            username: true,
+            image: true,
+        }
+    });
+    res.status(200).json(allUsers);
 });
 
 //@desc Get a single user
 //@route GET /users/:id
 //@access public
 const getUser = asyncHandler(async(req, res, err) => {
-    res.status(501).json({ error: "unimplemented" });
+    const { id } = req.params;
+    const asNum = Number(id);
+    if (isNaN(asNum)) {
+        res.status(404).json({ error: "User does not exist"});
+        return;
+    }
+    const user = await prisma.user.findUnique({
+        where: {
+            id: asNum,
+        }
+    });
+    if (user === null) {
+        res.status(404).json({ error: "User does not exist"});
+        return;
+    }
+    res.status(200).json(user);
 });
 
 //@desc Create a user
 //@route POST /users
 //@access public
 const createUser = asyncHandler(async(req, res, err) => {
-    res.status(501).json({ error: "unimplemented" });
+    const { email, name, username } = req.body;
+    if (!email || !name || !username) {
+        res.status(400).json({ error: "All fields required." });
+        return;
+    }
+    try {
+        const newUser = await prisma.user.create({
+            data: { email, name, username }
+        });
+        res.status(201).json(newUser);
+    } catch (e) {
+        res.status(400).json({ error: "Unable to create new user." }); // potentially due to unique fields
+    }
 });
 
 //@desc Update a user
 //@route PUT /users/:id
-//@access public
+//@access private
 const updateUser = asyncHandler(async(req, res, err) => {
-    res.status(501).json({ error: "unimplemented" });
+    const { id } = req.params;
+    const asNum = Number(id);
+    if (isNaN(asNum)) {
+        res.status(404).json({ error: "User does not exist." });
+        return;
+    }
+
+    const { email, name, image, bio } = req.body;
+    try {
+        const updatedUser = await prisma.user.update({
+            where: { id: asNum },
+            data: { email, name, image, bio }
+        })
+        res.status(200).json(updatedUser);
+    } catch (e) {
+        res.status(400).json({ error: "Unable to update user." });
+    }
 });
 
 //@desc Delete a user
 //@route POST /users/:id
-//@access public
+//@access private
 const deleteUser = asyncHandler(async(req, res, err) => {
-    res.status(501).json({ error: "unimplemented" });
+    const { id } = req.params;
+    const asNum = Number(id);
+    if (isNaN(asNum)) {
+        res.status(404).json({ error: "User does not exist." });
+        return;
+    }
+    try {
+        await prisma.user.delete({
+            where: { id: asNum }
+        });
+        res.sendStatus(200);
+    } catch (e) {
+        res.status(400).json({ error: "Delete request failed." });
+    }
 });
 
 module.exports = {
