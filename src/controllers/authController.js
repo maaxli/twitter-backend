3acc459fde693b1b997a0017446a8e6bed0d76d2
Max L <122556386+maaxli@users.noreply.github.com>
2023-12-30 19:16:57 -0500

    Implement email token authentication

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,17 @@
 const asycnHandler = require('express-async-handler');
 const { PrismaClient } = require('@prisma/client');
+const jwt = require('jsonwebtoken');
 
 const prisma = new PrismaClient();
 
-const EMAIL_TOKEN_EXPIRATION_MINIUTES = 10;
+const EMAIL_TOKEN_EXPIRATION_MINIUTES = 30;
+const JWT_EXPIRATION_HOURS = 12;
 
-//@desc Generate a db email token and send an email containing the token to the client
+/* Generate a db email token and send an email containing the token to the client
+ *
+ * @param email The email of the client
+ * @param emailTokenString The string email token
+ */
 async function generateDbEmailToken(email, emailTokenString) {
     const expiration = new Date(new Date().getTime() + EMAIL_TOKEN_EXPIRATION_MINIUTES * 60 * 1000);
     try {
@@ -27,7 +33,10 @@ async function generateDbEmailToken(email, emailTokenString) {
     }
 }
 
-// helper function for generating a db email token (string)
+/* Helper function for generating a db email token
+ *
+ * @return A unique (not currently in use) email token
+ */
 async function generateUniqueEmailToken() {
     let emailTokenString = "";
     let dbToken = null;
@@ -38,6 +47,22 @@ async function generateUniqueEmailToken() {
         });
     } while (dbToken !== null);
     return emailTokenString;
+}
+
+/* Helper function for generating a JWT
+ * 
+ * @param tokenId The ID of the corresponding token in the db
+ * @return JWT
+ */
+function generateJWT(tokenId) {
+    return jwt.sign(
+        { tokenId },
+        process.env.JWT_SECRET,
+        {
+            algorithm: "HS256",
+            expiresIn: "12h"
+        }
+    );
 }
 
 //@desc log in a user
@@ -74,10 +99,54 @@ const loginUser = asycnHandler(async (req, res, err) => {
 //@route /auth/authenticate
 //@access public
 const obtainJWT = asycnHandler(async (req, res, err) => {
-    // verify that stateful token is valid, delete if not
-    // generate JWT (use helper function)
-    // delete email token after generating JWT
-    res.json({ message: "request received! "});
+
+    // Extract information from client request
+    const { email, emailToken } = req.body;
+    if (!email || !emailToken) {
+        res.status(400).json({ error: "Missing fields" });
+    }
+    const dbEmailToken = await prisma.token.findUnique({
+        where: { emailToken: emailToken },
+        include: { user: true }
+    });
+
+    // Token validation
+    if (!dbEmailToken) {
+        res.sendStatus(401);
+        return;
+    }
+    if (dbEmailToken.user.email !== email) {
+        res.sendStatus(401);
+        return;
+    }
+    if (dbEmailToken.expiration < new Date()) {
+        await prisma.token.delete({
+            where: { emailToken: emailToken }
+        })
+        res.sendStatus(401);
+        return;
+    }
+
+    // Deleting used email token
+    await prisma.token.delete({
+        where: { emailToken: emailToken }
+    });
+    
+    // Storing a corresponding db token for the JWT
+    const expiration = new Date(new Date().getTime() + JWT_EXPIRATION_HOURS * 60 * 60 * 1000);
+    const dbJwtToken = await prisma.token.create({
+        data: {
+            type: "JWT",
+            expiration: expiration,
+            user: {
+                connect: { id: dbEmailToken.user.id }
+            }
+        }
+    });
+
+    // Generate JWT (TODO ideally stored in cookies)
+    const jwtToken = generateJWT(dbJwtToken.id);
+    res.json(jwtToken);
 });
 
 module.exports = {
